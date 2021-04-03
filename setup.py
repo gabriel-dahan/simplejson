815dbf8b08c1d9b1b9294c4506bceb452d6e9d87
thegabdoosan <gabrieldahan05@gmail.com>
2021-04-03 17:17:42 +0200

    Release

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'SimpleJson',
+    version = '1.0',
+    description = 'Simplify JSON',
+    author = 'Gabriel D.',
+    author_email = 'gabrieldahan05@gmail.com',
+    py_modules = ['simplejson'],
+)
